6625b647a79e06b0f9d9b317a98e8b67f4a893c9
faisal5467 <faisalabbas3120@gmail.com>
2024-09-15 20:02:43 +0500

    navigation change

--- App.js
+++ App.js
@@ -3,8 +3,22 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 const App = () => {
-  return <RootNavigator />;
+  
+  // return <RootNavigator />;
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+  
+     
+    }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="ProductScreen" component={ProductScreen} />
+      <Stack.Screen name="ProductDetailScreen" component={ProductDetailScreen} />
+    </Stack.Navigator>
+  );
 };
 
 export default App;
