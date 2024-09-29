89ce7edaeac6f363e89be9157e4e3130199d6ae7
faisal5467 <faisalabbas3120@gmail.com>
2024-09-29 19:08:32 +0500

    login delet

--- src/constants/BASE_URL_IMAGE/index.js
+++
@@ -1,4 +0,0 @@
-// export const BASE_URL_IMAGE = 'http://disciplineguru.co/backend';
-// export const BASE_URL_IMAGE = 'https://disciplineguru.co/test/';
-export const BASE_URL_IMAGE = 'https://betterself.cloud/test/';
-
